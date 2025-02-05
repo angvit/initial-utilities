8e687ea23b49e1ec0c3eeb183e52fa9debb755fa
angvit <angelo.vitalino@baruchmail.cuny.edu>
2025-02-04 20:46:50 -0500

    implementing wcat.c

---
+++ wcat/wcat.c
@@ -0,0 +1,26 @@
+#include <stdio.h> 
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+    if (argc == 1) {
+        fprintf(stderr, "usage: wcat <FILENAME...>\n");
+        exit(0);
+    }
+
+    for (int i=1; i<argc; i++){
+        FILE *fp = fopen(argv[i], "r");
+
+        if (fp == NULL) {
+            printf("wcat: cannot open file\n");
+            exit(1);
+        }
+        
+        char buff[10000];
+        
+        while (fgets(buff, sizeof(buff), fp) != NULL) {
+            printf("%s", buff);
+        }
+        fclose(fp);
+    }
+    return 0;
+}
